d627d1bc0821f8ab930fef47bb9244f7a9d4ddfa
Raphael <raphael.fnds@gmail.com>
2025-11-29 11:04:34 -0300

    docs_projeto_extensao

--- handlers/attendantHandler.js
+++ handlers/attendantHandler.js
@@ -2,7 +2,7 @@ module.exports = {
   handle() {
     const initialMessage = encodeURIComponent('Olá, fui redirecionado pelo atendimento de IA, preciso tirar uma dúvida');
     return {
-      response: `Por favor, clique no link para ser redirecionado: https://wa.me/554288768668?text=${initialMessage}`,
+      response: `Por favor, clique no link para ser redirecionado: https://wa.me/554299600556?text=${initialMessage}`,
       newState: null
     };
   }
